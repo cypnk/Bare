1ac28b1a4866efa53bc7fecd01f70f573b727f44
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-03-19 07:11:29 -0400

    Removed redundant abort check

--- index.php
+++ index.php
@@ -7821,11 +7821,6 @@ function request( string $event, array $hook, array $params ) : array {
 	// Check throttling
 	sessionThrottle();
 	
-	// Check for closed connection
-	if ( \connection_aborted() ) {
-		visitorAbort();
-	}
-	
 	$host	= getHost();
 	
 	// Empty host?
