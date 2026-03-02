3b3be356f75be40beede109a134f4105fa87dfb2
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2026-03-02 11:00:16 -0500

    Create bare.sqlite.sql

---
+++ cache/schema/bare.sqlite.sql
@@ -0,0 +1,170 @@
+-- Installation metadata
+CREATE TABLE IF NOT EXISTS schema_meta (
+	version TEXT NOT NULL PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	comments TEXT
+);
+
+CREATE TABLE IF NOT EXISTS maintenance_meta (
+	maintenance_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	settings TEXT NOT NULL DEFAULT '{}'
+);
+
+CREATE TRIGGER trg_update_maintenance_meta
+BEFORE UPDATE ON maintenance_meta
+FOR EACH ROW
+BEGIN
+	UPDATE maintenance_meta SET updated_at = CURRENT_TIMESTAMP
+		WHERE maintenance_id = NEW.maintenance_id;
+END;
+
+-- Cache tables
+CREATE TABLE caches (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	cache_id TEXT NOT NULL COLLATE NOCASE, 
+	ttl INTEGER NOT NULL, 
+	content TEXT NOT NULL COLLATE NOCASE, 
+	expires DATETIME DEFAULT NULL,
+	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+	updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+
+CREATE UNIQUE INDEX idx_caches_on_cache_id ON caches ( cache_id ASC );-- --
+CREATE INDEX idx_caches_on_expires ON caches ( expires DESC )
+	WHERE expires IS NOT NULL;-- --
+CREATE INDEX idx_caches_on_created ON caches ( created ASC );-- --
+CREATE INDEX idx_caches_on_updated ON caches ( updated );-- --
+
+-- Cache triggers
+CREATE TRIGGER cache_after_insert AFTER INSERT ON caches FOR EACH ROW 
+BEGIN
+	-- Generate expiration
+	UPDATE caches SET 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+	
+	-- Clear expired data
+	DELETE FROM caches WHERE 
+		strftime( '%s', expires ) < 
+		strftime( '%s', updated ) AND expires IS NOT NULL;
+END;-- --
+
+-- Change only update period when TTL is empty
+CREATE TRIGGER cache_after_update AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl = 0
+BEGIN
+	UPDATE caches SET updated = CURRENT_TIMESTAMP 
+		WHERE rowid = NEW.rowid;
+END;-- --
+
+-- Change expiration period when TTL exists
+CREATE TRIGGER cache_after_update_ttl AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl <> 0
+BEGIN
+	-- Change expiration
+	UPDATE caches SET updated = CURRENT_TIMESTAMP, 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+END;-- --
+
+-- Post content
+CREATE TABLE posts(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	post_path TEXT NOT NULL COLLATE NOCASE,
+	post_view TEXT NOT NULL COLLATE NOCASE,
+	post_bare TEXT NOT NULL COLLATE NOCASE, 
+	post_summary TEXT DEFAULT '' COLLATE NOCASE, 
+	post_type TEXT DEFAULT '' COLLATE NOCASE, 
+	updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	published DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+CREATE INDEX idx_post_updated ON posts( updated DESC );-- --
+CREATE INDEX idx_post_published ON posts( published DESC );-- --
+CREATE UNIQUE INDEX idx_post_path ON posts( post_path );-- --
+
+-- Tag tables
+CREATE TABLE tags (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	slug TEXT NOT NULL COLLATE NOCASE, 
+	term TEXT NOT NULL COLLATE NOCASE,
+	post_count INTEGER NOT NULL DEFAULT 0
+);-- --
+CREATE UNIQUE INDEX idx_tag_slug ON tags( slug ASC );-- --
+
+CREATE TABLE post_tags(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	post_id INTEGER NOT NULL REFERENCES posts( id ) 
+		ON DELETE CASCADE,
+	tag_slug TEXT NOT NULL COLLATE NOCASE
+);-- --
+CREATE INDEX idx_post_tags_id ON post_tags( post_id );-- --
+CREATE INDEX idx_post_tags_slug ON post_tags( tag_slug );-- --
+CREATE UNIQUE INDEX idx_post_tags ON post_tags( post_id, tag_slug );-- --
+
+-- Tag triggers
+CREATE TRIGGER tag_after_insert AFTER INSERT ON post_tags FOR EACH ROW 
+BEGIN
+	UPDATE tags SET post_count = ( post_count + 1 )
+		WHERE slug = NEW.tag_slug;
+END;-- --
+
+CREATE TRIGGER tag_before_delete BEFORE DELETE ON post_tags FOR EACH ROW 
+BEGIN
+	UPDATE tags SET post_count = ( post_count - 1 )
+		WHERE slug = OLD.tag_slug;
+END;-- --
+
+-- Searching
+CREATE VIRTUAL TABLE post_search 
+	USING fts4( body, tokenize=unicode61 );-- --
+
+CREATE TRIGGER post_insert AFTER INSERT ON posts FOR EACH ROW 
+BEGIN
+	INSERT INTO post_search( docid, body ) 
+		VALUES ( NEW.id, NEW.post_bare );
+END;-- --
+
+CREATE TRIGGER post_before_update BEFORE UPDATE ON posts FOR EACH ROW
+BEGIN
+	DELETE FROM post_tags WHERE post_id = OLD.id;
+END;-- --
+
+CREATE TRIGGER post_update AFTER UPDATE ON posts FOR EACH ROW 
+BEGIN
+	UPDATE post_search SET body = NEW.post_bare 
+		WHERE docid = NEW.id;
+END;-- --
+
+CREATE TRIGGER post_delete BEFORE DELETE ON posts FOR EACH ROW 
+BEGIN
+	DELETE FROM post_search WHERE docid = OLD.id;
+	DELETE FROM post_tags WHERE post_id = OLD.id;
+END;-- --
+
+
+-- Post info for sibling posts
+CREATE VIEW post_siblings AS SELECT DISTINCT 
+	p.post_path AS post_path, 
+	( SELECT post_path FROM posts prev
+		WHERE prev.published IS NOT NULL AND
+			strftime( '%s', prev.published ) <= 
+			strftime( '%s', p.published ) 
+			AND prev.post_path IS NOT p.post_path
+			ORDER BY prev.published DESC LIMIT 1 
+	) AS prev_path, 
+	
+	-- Next published sibling
+	( SELECT post_path FROM posts nxt 
+		WHERE nxt.published IS NOT NULL AND 
+			strftime( '%s', nxt.published ) > 
+			strftime( '%s', p.published ) 
+			AND nxt.post_path IS NOT p.post_path
+			ORDER BY nxt.published ASC LIMIT 1 
+	) AS next_path
+	
+	FROM posts p;
